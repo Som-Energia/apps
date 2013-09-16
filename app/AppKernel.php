ace599f7a95e7be6d2fbd00e5d9495d13fe0c1fc
David Romaní <david@flux.cat>
2013-09-16 18:07:53 +0200

    budget 20130719 - part2 - step 0.1 - composer update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,10 +20,6 @@ class AppKernel extends Kernel
 
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
 
