4495256817a896e4f9f88730d67b54e3e7e26076
David Romaní <david@flux.cat>
2013-09-17 09:21:05 +0200

    budget 20130719 - part2 - step 0.7 - GrupoLocal Entity created

---
+++ src/SomEnergia/GrupoLocalBundle/Entity/GrupoLocal.php
@@ -0,0 +1,61 @@
+<?php
+namespace SomEnergia\GrupoLocalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="somenergia_grupo_local")
+ * @ORM\Entity(repositoryClass="SomEnergia\GrupoLocalBundle\Repository\GrupoLocalRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class GrupoLocal
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false, unique=true)
+     */
+    protected $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return GrupoLocal
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
