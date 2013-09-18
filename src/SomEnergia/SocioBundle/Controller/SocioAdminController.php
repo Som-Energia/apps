f2cda3d7758acfbde526e55d495d3f8d748f3391
David Romaní <david@flux.cat>
2013-09-18 18:10:55 +0200

    budget 20130719 - part2 - step 2.4 - Socios Bundle get related list refactor

--- src/SomEnergia/SocioBundle/Controller/SocioAdminController.php
+++ src/SomEnergia/SocioBundle/Controller/SocioAdminController.php
@@ -2,12 +2,20 @@
 namespace SomEnergia\SocioBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use SomEnergia\GrupoLocalBundle\Entity\GrupoLocal;
 
 class SocioAdminController extends Controller
 {
     public function relatedListAction()
     {
+        $glid = 1;
+        $em = $this->getDoctrine()->getManager();
+        /** @var GrupoLocal $grupoLocal */
+        $grupoLocal = $em->getRepository('GrupoLocalBundle:GrupoLocal')->find($glid);
+
         return $this->render('SocioBundle:Admin:relatedlist.html.twig', array(
+            'glid' => $glid,
+            'grupoLocal' => $grupoLocal,
         ));
     }
 }
