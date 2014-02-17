1edc73c579b41548472053a8378e2fd72b5a9e81
David Romaní <david@flux.cat>
2014-02-17 16:11:23 +0100

    step 6.8 - security simplified, removed unnecessary config

--- src/SomEnergia/MainBundle/Controller/DefaultController.php
+++ src/SomEnergia/MainBundle/Controller/DefaultController.php
@@ -9,6 +9,5 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->redirect($this->generateUrl('sonata_admin_dashboard'));
-        //return $this->render('MainBundle:Default:index.html.twig', array('name' => ''));
     }
 }
