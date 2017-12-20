dc97bd65ab31885c58aaac5b0f4c261f712a1c5c
David Romaní <david@flux.cat>
2017-12-20 16:21:22 +0100

    add developer scripts

---
+++ app/scripts/developer/run-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    phpunit -c app/
+  else
+    if [ "$1" = "cc" -o "$1" = "coverage" ]
+          then
+            if [ "$1" = "cc" ]
+              then
+                php app/console ca:cl --env=test && phpunit -c app/
+              else
+                phpunit -c app/ --coverage-text
+            fi
+          else
+            echo "Argument error! Available argument options: 'cc' or 'coverage'"
+        fi
+fi
+
